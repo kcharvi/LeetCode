ab97b69112d1ccfa7991ccd497d6112b16296956
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-03 22:08:42 +0530

    Time: 3 ms (84.72%), Space: 45.3 MB (61.92%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int candy(int[] ratings) {
+        boolean flag= true;
+        int candies[] = new int[ratings.length];
+        for(int i =0; i<candies.length;i++){
+            candies[i]=1;
+        }
+        for(int i = 1; i < ratings.length; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        for(int i = ratings.length-2; i >=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i+1]+1, candies[i]);
+            }
+        }
+
+        int sum = 0;
+        for(int i=0;i<candies.length; i++)
+            sum+=candies[i];
+        
+        return sum;
+    }
+}
