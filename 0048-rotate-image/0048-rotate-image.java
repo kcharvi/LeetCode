4b4390ab4f3fd5b16d649605b2a8362b0cb75811
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-10 22:25:39 +0530

    Time: 0 ms (100%), Space: 42 MB (46.9%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        Collections.reverse(Arrays.asList(matrix));
+        for(int i=1;i<matrix.length;i++){
+            for(int j=0;j<i;j++){
+                int temp = matrix[i][j];
+    		    matrix[i][j]=matrix[j][i];
+    		    matrix[j][i]=temp;
+            }
+        }
+    }
+}
