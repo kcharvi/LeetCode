a743b44634ca01c4a409473675e08b93d0345a7a
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-21 16:31:36 +0530

    Time: 0 ms (100%), Space: 43.9 MB (87.92%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -13,25 +13,51 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode buildTree(int[] inorder, int[] postorder) {
+//         if(postorder.length == 0 || inorder.length==0)return null;
+//         int v=postorder[postorder.length-1];
+//         TreeNode root = new TreeNode(v);
+//         int idx=0;
+//         for(int i =0; i<inorder.length; i++){
+//             if(inorder[i]==v){
+//                 idx = i;
+//                 break;
+//             }
+//         }
+        
+//         root.left = buildTree(Arrays.copyOfRange(inorder, 0, idx),
+//                               Arrays.copyOfRange(postorder, 0, idx));
+//         root.right = buildTree(Arrays.copyOfRange(inorder, idx + 1, inorder.length),
+//                                Arrays.copyOfRange(postorder, idx, postorder.length - 1));
+        
+//         return root;
+//     }
+    
+// }
+
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        if(postorder.length == 0 || inorder.length==0)return null;
-        int v=postorder[postorder.length-1];
-        TreeNode root = new TreeNode(v);
-        int idx=0;
-        for(int i =0; i<inorder.length; i++){
-            if(inorder[i]==v){
-                idx = i;
+        int n = inorder.length;
+        int[] idx = new int[] { n - 1 };
+        return construct2(inorder, postorder, 0, n - 1, idx);
+    }
+
+    private TreeNode construct2(int[] inorder, int[] postorder, int start, int end, int[] idx)  {
+        if (start > end) {
+            return null;
+        }
+        int rootVal = postorder[idx[0]];
+        int i;
+        for (i = end; i >= start; i--) {
+            if (inorder[i] == rootVal) {
                 break;
             }
         }
-        
-        root.left = buildTree(Arrays.copyOfRange(inorder, 0, idx),
-                              Arrays.copyOfRange(postorder, 0, idx));
-        root.right = buildTree(Arrays.copyOfRange(inorder, idx + 1, inorder.length),
-                               Arrays.copyOfRange(postorder, idx, postorder.length - 1));
-        
+        idx[0]--;
+        TreeNode root = new TreeNode(rootVal);
+        root.right = construct2(inorder, postorder, i + 1, end, idx); 
+        root.left = construct2(inorder, postorder, start, i - 1, idx); 
         return root;
     }
-    
 }
