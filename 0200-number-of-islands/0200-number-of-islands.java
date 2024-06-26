2aa964cd7ccda1319de5bb6bd499de0184cc70de
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-26 13:03:42 +0530

    Time: 6 ms (24.31%), Space: 49.5 MB (44.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -4,20 +4,34 @@ class Solution {
         int islands = 0;
         int rows = grid.length;
         int cols = grid[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+
         for(int i=0; i<rows; i++)
             for(int j=0; j<cols; j++)
                 if(grid[i][j] == '1'){
-                    dfs(i, j, grid);
+                    bfs(i, j, grid);
                     islands++;
                 }
         return islands;
     }
-    public void dfs(int i, int j, char[][] grid){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] != '1')return;
-        grid[i][j]='2';
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid);
+    public void bfs(int i, int j, char[][] grid){
+        Queue<int[]> q = new LinkedList<>();
+        grid[i][j] = '2';
+        q.add(new int[]{i,j});
+        int rows = grid.length;
+        int cols = grid[0].length;
+        while(!q.isEmpty()){
+            int[] arr = q.poll();
+            int[][] directions = {{1, 0},{-1, 0},{0, 1},{0, -1}};
+            for(int[] dir : directions){
+                int r = arr[0] + dir[0];
+                int c = arr[1] + dir[1];
+                if(r>=0 && r<rows && c>=0 && c<cols)
+                    if(grid[r][c] == '1'){
+                        q.add(new int[]{r,c});
+                        grid[r][c] = '2';
+                    }
+            }
+        }
     }
 }
