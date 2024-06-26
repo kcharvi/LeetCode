b0011cfbbf55d8429e73fb57388e89f583875954
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-26 15:13:14 +0530

    Time: 2 ms (86.13%), Space: 45.4 MB (41.17%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -4,27 +4,17 @@ class Solution {
         int cols = board[0].length;
 
         // Check the edges of the board, and mark all of them as '#'
-        for(int i = 0; i<rows; i++) {
-            if(board[i][0] == 'O')
-                dfs(board, i, 0);
-            if(board[i][cols - 1] == 'O')
-                dfs(board, i, cols - 1);
-        }
-        for (int j = 0; j < cols; j++) {
-            if(board[0][j] == 'O') 
-                dfs(board, 0, j);
-            if(board[rows - 1][j] == 'O') 
-                dfs(board, rows - 1, j);
-        }
-
-        for(int i =0; i<rows;i++)
-            for(int j =0; j<cols;j++)
-                if(board[i][j]=='O'){
-                    board[i][j]='X';
-                } 
-                else if(board[i][j]=='#'){
-                    board[i][j]='O';
-                }
+        for(int i = 0; i<rows; i++) 
+            for (int j = 0; j < cols; j++)
+                if (i * j == 0 || i == rows - 1 || j == cols - 1)
+                    dfs(board, i, j);
+                    
+        for (char[] row : board)
+            for (int i = 0; i < row.length; ++i)
+                if (row[i] == '#')
+                    row[i] = 'O';
+                else if (row[i] == 'O')
+                    row[i] = 'X';
     }
     public void dfs(char[][] board, int i, int j){
         if(i<0 || j<0 ||
