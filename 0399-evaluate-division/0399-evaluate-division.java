8efe1e42ec7a23a90dcd46c3858ca7351fe567f1
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-28 12:11:05 +0530

    Time: 2 ms (29.84%), Space: 42.2 MB (33.34%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,28 +1,22 @@
-import java.util.*;
-
 class Solution {
     class Pair<K, V> {
-        private K key;
-        private V value;
-
+        K key;
+        V value;
         public Pair(K key, V value) {
             this.key = key;
             this.value = value;
         }
-
         public K getKey() {
             return key;
         }
-
         public V getValue() {
             return value;
         }
     }
 
-    HashMap<String, Map<String, Double>> adjmap = new HashMap<>();
-
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-        int i = 0;
+    Map<String, Map<String, Double>> adjmap = new HashMap();
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {    
+        int i=0 ;
         for (List<String> str : equations) {
             String a = str.get(0);
             String b = str.get(1);
@@ -34,45 +28,40 @@ class Solution {
             adjmap.get(b).put(a, 1 / values[i]);
             i++;
         }
-
+        
         double[] ans = new double[queries.size()];
-        for (int j = 0; j < queries.size(); j++) {
-            ans[j] = bfs(queries.get(j).get(0), queries.get(j).get(1));
+        for(int j=0; j<queries.size();j++){
+            ans[j] = bfs(queries.get(j).get(0), 
+                         queries.get(j).get(1));
         }
         return ans;
     }
-
-    public double bfs(String src, String target) {
-        if (!adjmap.containsKey(src) || !adjmap.containsKey(target)) return -1.0;
-        if (src.equals(target)) return 1.0;
-
-        Queue<Pair<String, Double>> q = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
-
+    public double bfs(String src, String target){
+        if(!adjmap.containsKey(src) || !adjmap.containsKey(target))return -1;
+        
+        Queue<Pair<String, Double>> q = new LinkedList();
+        Set<String> visit = new HashSet();
         q.add(new Pair<>(src, 1.0));
-        visited.add(src);
+        visit.add(src);
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             Pair<String, Double> curr = q.poll();
             String currNode = curr.getKey();
             Double currValue = curr.getValue();
 
-            if (currNode.equals(target)) {
+            if(currNode.equals(target))
                 return currValue;
-            }
-
-            Map<String, Double> neighbors = adjmap.get(currNode);
-            for (Map.Entry<String, Double> entry : neighbors.entrySet()) {
+            Map<String, Double> nei = adjmap.get(currNode);
+            for (Map.Entry<String, Double> entry : nei.entrySet()) {
                 String neighbor = entry.getKey();
                 Double weight = entry.getValue();
-
-                if (!visited.contains(neighbor)) {
+                if (!visit.contains(neighbor)) {
                     q.add(new Pair<>(neighbor, currValue * weight));
-                    visited.add(neighbor);
+                    visit.add(neighbor);
                 }
             }
         }
-
         return -1.0;
+
     }
 }
