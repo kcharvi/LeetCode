c4bce47bbb2bf61507b1e9f2aa96f89bd7243b19
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-06-30 21:22:25 +0530

    Time: 1 ms (66.89%), Space: 42.1 MB (45.91%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,37 +1,34 @@
 class Solution {
+    
     public List<String> letterCombinations(String digits) {
-        List <String> list=new ArrayList<>();
-        if(digits=="" || digits==null || digits.length()==0){
-            return list;
-        }
-        Map<Character,String> hash=new HashMap<>();
-       hash.put('2',"abc");
-       hash.put('3',"def");
-       hash.put('4',"ghi");
-       hash.put('5',"jkl");
-       hash.put('6',"mno");
-       hash.put('7',"pqrs");
-       hash.put('8',"tuv");
-       hash.put('9',"wxyz");
-       String [] strArr=new String[digits.length()];
-       for(int i=0;i<digits.length();i++){
-           strArr[i]=hash.get(digits.charAt(i));
-       }
-       String [] str=strArr[0].split("");
-       for(int i=1;i<strArr.length;i++){
-             str= getStrings(str,strArr[i]);
-       }
-       return Arrays.asList(str);
+        if(digits=="" || digits==null || digits.length()==0)
+            return new ArrayList<>();
         
+        Map<Character, String> map = new HashMap();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+        
+        String[] strs = new String[digits.length()];
+        for(int i =0; i<digits.length();i++)
+            strs[i]=map.get(digits.charAt(i));
+        
+        String[] arr = strs[0].split(""); // 2 - abc   arr=["a","b","c"]
+        for(int i=1; i<strs.length; i++)
+            arr = getStrings(arr, strs[i]);   // (arr=["a","b","c"], strs=["def"])
+        return Arrays.asList(arr);
     }
-    public String [] getStrings(String [] strArr,String str2){
-        String [] str=new String[strArr.length*str2.length()];
+    public String[] getStrings(String[] arr, String str){
+        String[] temp = new String[arr.length * str.length()];  // arr=3 * str=3  which is 9
         int k=0;
-            for(int i=0;i<strArr.length;i++){
-                for(int j=0;j<str2.length();j++){
-                  str[k++] = strArr[i].concat(String.valueOf(str2.charAt(j)));
-                } 
-            }
-            return str;
+        for(int i=0;i<arr.length;i++)
+            for(int j=0; j<str.length();j++)
+                temp[k++] = arr[i].concat(String.valueOf(str.charAt(j)));
+        return temp;
     }
 }
