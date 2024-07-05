b5df55384ec8684b9e80efed40f54b4d5d931315
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-05 22:42:49 +0530

    Time: 0 ms (100%), Space: 45.7 MB (55.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,26 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0)return new int[]{-1, -1};
-        int l = 0;
-        int r = nums.length-1;
-        int mid = -1;
-        while(l<=r){
-            mid = l+ (r-l)/2;
-            if(target == nums[mid])break;
-            if(target > nums[mid])l=mid+1;
-            else r = mid-1;
+        int[] result = new int[2];
+        result[0] = findStartingIndex(nums, target);
+        result[1] = findEndingIndex(nums, target);
+        return result;
+    }
+
+    private int findStartingIndex(int[] nums, int target) {
+        int index = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == target) index = mid;
         }
-        if(nums[mid] != target)return new int[]{-1, -1};
-        int strt = mid;
-        while(strt>=0){
-            if(nums[strt] == target)strt--;
-            else break;
+        return index;
+    }
+
+    private int findEndingIndex(int[] nums, int target) {
+        int index = -1;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == target) index = mid;
         }
-        int end = mid;
-        while(end<nums.length){
-            if(nums[end] == target)end++;
-            else break;
-        }
-        return new int[]{strt+1, end-1};
+        return index;
     }
 }
