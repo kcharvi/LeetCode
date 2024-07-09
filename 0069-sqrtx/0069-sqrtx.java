79c35bd2c5fba0bf5ac53cf8a77fe3217fcdacbb
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-09 21:53:53 +0530

    Time: 1 ms (86.61%), Space: 40.7 MB (55.34%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1)return x;
+        int l = 1;
+        int r = x;
+        int mid = -1;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if((long) mid*mid > (long) x)r = mid-1;
+            else if(mid*mid == x)return mid;
+            else l=mid+1;
+        }
+        return Math.round(r);
+    }
+}
