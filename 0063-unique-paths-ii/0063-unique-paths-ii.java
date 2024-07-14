93ef1abfc1209d4ec6fd933bdc9ae3fc018e912b
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-14 16:29:48 +0530

    Time: 0 ms (100%), Space: 41.4 MB (67.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,22 @@
 class Solution {
-    int m, n, dp[][], obs[][];
-    public int uniquePathsWithObstacles(int[][] obs) {
-        m = obs.length;
-        n = obs[0].length;
-        this.obs = obs;
-        if(obs[0][0] + obs[m-1][n-1] > 0) return 0;
-        dp = new int[m][n];
-        for(int i = 0; i < m; i++){
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if((obstacleGrid[0][0] + obstacleGrid[m-1][n-1])>0)return 0;
+        int[][] dp = new int[m][n];
+        for(int i =0; i<m; i++)
             Arrays.fill(dp[i], -1);
-        }
-        return dfs(0, 0);
+        return dfs(0,0, dp, obstacleGrid, m, n);
     }
-    int dfs(int i, int j){
-        if(i == m-1 && j == n-1){
+    int dfs(int i, int j, int[][] dp, int[][] obstacleGrid, int m, int n){
+        if(i==m-1 && j==n-1)
             return 1;
-        }
-        if(i > m-1 || j > n-1 || i < 0 || j < 0 || obs[i][j] == 1){
+        if(i<0 || j<0 || i>=m || j>=n || obstacleGrid[i][j]==1)
             return 0;
-        }
-        if(dp[i][j] != -1){
+        if(dp[i][j]!=-1)
             return dp[i][j];
-        }
-        dp[i][j] = dfs(i+1, j) + dfs(i, j+1);
+        dp[i][j] = dfs(i+1, j, dp, obstacleGrid, m, n)
+                 + dfs(i, j+1, dp, obstacleGrid, m, n);
         return dp[i][j];
     }
 }
