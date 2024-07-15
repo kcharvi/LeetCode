8eb2a6d74728f4cb8acd9eff16764ac434bdd459
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-15 21:10:17 +0530

    Time: 0 ms (100%), Space: 42.3 MB (18.45%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,42 +1,30 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        int x = s1.length();
-        int y = s2.length();
-        int z = s3.length();
-        if(x+y!=z){
+        if (s1.length() + s2.length() != s3.length()){
             return false;
-        } 
-        //Table Creation
-        boolean dp[][]=new boolean[x+1][y+1];
-        dp[0][0]=true;
+        }
+        Boolean[][] dp = new Boolean[s1.length() + 1][s2.length() + 1];
+        return backtrack(s1,s2,0,0,s3,dp);
+    }
 
-        //Base Case Initialization
-        for(int i=1;i<=x;i++){
-            if(s1.charAt(i-1) == s3.charAt(i-1)){
-                dp[i][0]=true;
-            } 
-            else break;
+    public boolean backtrack(String s1,String s2,int i,int j,String s3,Boolean[][] dp){
+        if (i == s1.length() && j == s2.length()){
+            return true;
         }
 
-        for(int j=1;j<=y;j++){
-            if(s2.charAt(j-1) == s3.charAt(j-1)){
-                dp[0][j]=true;
-            } 
-            else break; 
+        if (dp[i][j] != null){
+            return dp[i][j];
         }
 
-        //Fill - Bottom Up
-        for(int i=1;i<=x;i++){
-            for(int j=1;j<=y;j++){
-                if(s1.charAt(i-1) == s3.charAt(i+j-1)){
-                    dp[i][j] = dp[i-1][j];
-                } 
-                if(!dp[i][j] && s2.charAt(j-1) == s3.charAt(i+j-1)){
-                    dp[i][j] = dp[i][j-1];
-                } 
-                //!dp[i][j] : so that i will go for 2nd condition only if 1st if statement doesn't satisfy the conditons.
-            }
+        boolean left = false;
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)){
+            left |= backtrack(s1,s2,i + 1,j,s3,dp);
         }
-        return dp[x][y];
+
+        boolean right = false;
+        if (!left && j < s2.length() && s2.charAt(j) == s3.charAt(i + j)){
+            right |= backtrack(s1,s2,i,j + 1,s3,dp);
+        }
+        return dp[i][j] = left || right;
     }
 }
