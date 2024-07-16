88165c6b907b1504920d819fbca3e5a4549a56f9
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-16 13:40:55 +0530

    Time: 7 ms (29.01%), Space: 41.8 MB (33.32%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,30 +1,31 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()){
-            return false;
+        if (s1.length() + s2.length() != s3.length()) return false;
+        
+        int m = s1.length();
+        int n = s2.length();
+        
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        
+        // Initialize the first column
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
         }
-        Boolean[][] dp = new Boolean[s1.length() + 1][s2.length() + 1];
-        return backtrack(s1,s2,0,0,s3,dp);
-    }
-
-    public boolean backtrack(String s1,String s2,int i,int j,String s3,Boolean[][] dp){
-        if (i == s1.length() && j == s2.length()){
-            return true;
+        
+        // Initialize the first row
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
         }
-
-        if (dp[i][j] != null){
-            return dp[i][j];
+        
+        // Fill the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (s3.charAt(i + j - 1) == s1.charAt(i - 1) && dp[i - 1][j]) ||
+                           (s3.charAt(i + j - 1) == s2.charAt(j - 1) && dp[i][j - 1]);
+            }
         }
-
-        boolean left = false;
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(i + j)){
-            left |= backtrack(s1,s2,i + 1,j,s3,dp);
-        }
-
-        boolean right = false;
-        if (!left && j < s2.length() && s2.charAt(j) == s3.charAt(i + j)){
-            right |= backtrack(s1,s2,i,j + 1,s3,dp);
-        }
-        return dp[i][j] = left || right;
+        
+        return dp[m][n];
     }
 }
