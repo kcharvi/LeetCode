6cb24a00633a81c6e25c0454cb7377807932704a
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-16 11:34:50 +0530

    Time: 5 ms (65.18%), Space: 44.8 MB (52.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,23 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        if(word1 == "" || word2 == "")
+            return Math.max(word1.length(), word2.length());
+            
         int[][] dp = new int[word1.length()+1][word2.length()+1];
-        for(int i=0; i<=word2.length();i++){
-            dp[0][i] = i;
-        }
-        for(int i =0; i<=word1.length();i++){
+
+        for(int i=0; i<=word1.length(); i++)
             dp[i][0] = i;
-        }
-        for(int i =1 ; i<=word1.length();i++){
-            for(int j=1;j<=word2.length();j++){
-                // if(i==0){
-                //     dp[i][j]=j;
-                // }
-                // else if(j==0){
-                //     dp[i][j]=i;
-                // }
-                if( word1.charAt(i-1) == word2.charAt(j-1) ){
+
+        for(int i=0; i<=word2.length(); i++)
+            dp[0][i] = i;
+
+        for(int i=1; i<=word1.length(); i++)
+            for(int j=1; j<=word2.length(); j++)
+                if(word1.charAt(i-1) == word2.charAt(j-1))
                     dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    int x = Math.min(dp[i-1][j]+1, dp[i][j-1]+1);
-                    dp[i][j] = Math.min(x, dp[i - 1][j - 1] + 1);
-                }
-            }
-        }
+                else
+                    dp[i][j] = 1+ Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i - 1][j - 1]);
+
         return dp[word1.length()][word2.length()];
     }
 }
