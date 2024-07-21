1dd4c1484f66ab3a18df8126137ae402603ce739
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-21 22:03:54 +0530

    Time: 4 ms (71.74%), Space: 42.4 MB (74%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,31 +1,14 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashMap<String,Boolean> hash = new HashMap<>();
-        return f("",s,wordDict,hash);
-    }
-    public boolean f(String cur,String target,List<String> wordDict,HashMap<String,Boolean> hash){
-        if(cur.length() == target.length()){
-            if(cur.equals(target)){
-                return true;
-            } 
-        }
-        if(hash.containsKey(cur)){
-            return hash.get(cur);
-        }
-        for(int i = 0; i<wordDict.size(); i++){
-            String j = wordDict.get(i);
-            String u = cur + j;
-            if(u.length()<=target.length()){
-                String y = target.substring(0,u.length());
-                if(u.equals(y)){
-                    if(f(cur+wordDict.get(i),target,wordDict,hash)){
-                        hash.put(cur,true);
-                        return true;
-                    }   
-                }
+        boolean[] dp = new boolean[s.length()+1];
+        dp[s.length()] = true;
+        for(int i=s.length()-1; i>=0; i--){
+            for(String w: wordDict){
+                if(i+w.length() <= s.length() && w.equals(s.substring(i, i+w.length())))
+                    dp[i] = dp[i+w.length()];
+                if(dp[i])break;
             }
-        }
-        hash.put(cur,false);
-        return false;
+        }   
+        return dp[0];
     }
 }
