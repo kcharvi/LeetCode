ed873f4a4b72b0e08e788c69b036b90d3323e122
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-07-25 17:57:07 +0530

    Time: 4 ms (71.27%), Space: 42.6 MB (67.46%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        boolean[] dp = new boolean[s.length()+1];
-        dp[s.length()] = true;
-        for(int i=s.length()-1; i>=0; i--){
-            for(String w: wordDict){
-                if(i+w.length() <= s.length() && w.equals(s.substring(i, i+w.length())))
-                    dp[i] = dp[i+w.length()];
+        int Slen = s.length();
+        boolean[] dp = new boolean[Slen+1];
+        dp[Slen]=true;
+
+        for(int i=Slen;i>=0; i--){
+            for(int j=0; j<wordDict.size(); j++){
+                int Wlen = wordDict.get(j).length();
+                String W = wordDict.get(j);
+                if((i+Wlen) <= Slen && W.equals(s.substring(i, i+Wlen)))
+                    dp[i] = dp[i+Wlen];
                 if(dp[i])break;
             }
-        }   
+        }
         return dp[0];
     }
 }
