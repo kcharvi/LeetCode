4f67d01047251eba52694715b07fc9b29f907b78
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-09-03 08:47:37 +0530

    Time: 63 ms (38.07%), Space: 43.5 MB (32.74%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,19 +1,17 @@
 class Solution {
     public int numSquares(int n) {
-        int[] memo = new int[10001];
-        Arrays.fill(memo, -1);
-        return dp(n, memo);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
     }
-
-    private int dp(int num, int[] memo) {
-        if (num < 0) return 10000;
-        if (num == 0) return 0;
-        if (memo[num] != -1) return memo[num];
-        int mini = 100000;
-        for (int i = 1; i <= 100; i++) {
-            mini = Math.min(mini, 1 + dp(num - i * i, memo));
+    int helper(int n, int[] dp){
+        if(dp[n]!=-1)return dp[n];
+        if(n==0)return 0;
+        int min = Integer.MAX_VALUE;
+        for(int i=1; i*i<=n; i++){
+            min = Math.min(min, helper(n-i*i, dp)+1);
         }
-        memo[num] = mini;
-        return mini;
+        dp[n] = min;
+        return min;
     }
 }
