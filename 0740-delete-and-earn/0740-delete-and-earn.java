4430db3337dcc9815ff1d29bfd499412d1cdd060
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-10-08 16:12:13 -0400

    Time: 5 ms (68.74%), Space: 45.1 MB (26.58%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,17 +1,25 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        int maxValue = 0;
-        for(int i = 0; i < nums.length; i++)
-         maxValue = Math.max(maxValue, nums[i]);
+        Arrays.sort(nums);
+        int n = nums.length;                
+        int maxx=0;
+        for(int i = 0; i < n; i++)
+            maxx = Math.max(maxx, nums[i]);
+        
+        int[] dp = new int[maxx+1];
 
-        int dp[] = new int[maxValue + 1];
-        for(int i = 0; i < nums.length; i++)
-         dp[nums[i]] += nums[i];
+        for(int i=0; i<n; i++)  
+            dp[nums[i]] += nums[i];
 
         dp[1] = Math.max(dp[0], dp[1]);
-        for(int i = 2; i < dp.length; i++)
-         dp[i] = Math.max(dp[i - 1], (dp[i] + dp[i - 2]) );
+        
+        for(int i=2; i<=maxx; i++){
+            dp[i] = Math.max(dp[i]+dp[i-2], dp[i-1]);
+        }
 
-        return dp[maxValue]; 
+        return dp[maxx];
+        // nums = [2,2,3,3,3,4]
+        // dp =   [ 0, 0, 2+2, 3+3+3, 4] 
+
     }
 }
