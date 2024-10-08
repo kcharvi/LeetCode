56f1dfbb43e57d115c1ab9634c0efea55f4d1d2c
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-10-08 16:13:52 -0400

    Time: 0 ms (100%), Space: 40.4 MB (37.78%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,6 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        if(m==0 ||n==0)return 0;
+        if(m==0 || n==0)return 0;
         if(m==1 || n==1)return 1;
         int[][] dp = new int[m][n];
         Arrays.fill(dp[0], 1);
