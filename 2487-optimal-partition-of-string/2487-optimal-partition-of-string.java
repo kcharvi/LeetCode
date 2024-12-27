8d7665189832e13bd0abe7fc7ccbe5b8423f0ce2
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2024-12-26 19:45:55 -0500

    Time: 31 ms (60.94%), Space: 45.7 MB (27%) - LeetHub

--- 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.java
@@ -1,24 +1,36 @@
 class Solution {
     public int partitionString(String s) {
-        int n = s.length();
-        int[] max_lengths = new int[n];
-        for(int i=0; i<n; i++){
-            HashSet set = new HashSet<>();
-            for(int j=i; j<n; j++){
-                if(set.contains(s.charAt(j))) break;
-                set.add(s.charAt(j));
+        // int n = s.length();
+        // int[] max_lengths = new int[n];
+        // for(int i=0; i<n; i++){
+        //     HashSet set = new HashSet<>();
+        //     for(int j=i; j<n; j++){
+        //         if(set.contains(s.charAt(j))) break;
+        //         set.add(s.charAt(j));
+        //     }
+        //     max_lengths[i] = set.size();
+        //     set.clear();
+        // }
+        // int splits = 0;
+        // int i = 0;
+        
+        // while (i < n) {
+        //     splits++;
+        //     i += max_lengths[i];
+        // }
+        
+        // return splits;
+
+
+        HashSet set = new HashSet();
+        int ans = 1;
+        for(char c: s.toCharArray()){
+            if(set.contains(c)){
+                ans++;
+                set.clear();
             }
-            max_lengths[i] = set.size();
-            set.clear();
+            set.add(c);
         }
-        int splits = 0;
-        int i = 0;
-        
-        while (i < n) {
-            splits++;
-            i += max_lengths[i];
-        }
-        
-        return splits;
+        return ans;
     }
 }
