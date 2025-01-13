002fbc47b512b6325b36a2596d39d9d8042e4506
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-01-12 19:51:46 -0500

    Time: 8 ms (86.92%), Space: 46.9 MB (14.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,32 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        if(intervals.length == 1)
+            return intervals;
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        List<int[]> list = new LinkedList<>();
-
-        int n=intervals.length;
-        int strt=intervals[0][0];
-        int end=intervals[0][1];
-
-        for(int i=1;i<n;i++){
-            if(end>=intervals[i][0]){
-                if(end<intervals[i][1]){
-                    end=intervals[i][1];
-                }
-                continue;
+        int idx = 0;
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[idx][1] >= intervals[i][0]){
+                intervals[idx][1] = Math.max(intervals[i][1], intervals[idx][1]);
             }
-            list.add(new int[]{strt,end});
-            strt=intervals[i][0];
-            end=intervals[i][1];
+            else{
+                idx++;
+                intervals[idx] = intervals[i];
+            }
         }
-        list.add(new int[] { strt, end });
-        int[][] ans = new int[list.size()][2];
-        int j=0;
-        for(int[] i:list){
-            ans[j][0]=i[0];
-            ans[j][1]=i[1];
-            j++;
-        }
-        return ans;
+        return Arrays.copyOfRange(intervals, 0, idx+1);
     }
 }
