4e097e5cc4704bba7af93aa226cea4b47c053cfa
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-02-16 13:46:26 -0500

    Time: 6 ms (74.68%), Space: 45.2 MB (69.8%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,90 +1,87 @@
+// DFS Solution
 // class Solution {
+//     HashMap<Integer, ArrayList<Integer>> map = new HashMap();
 //     public boolean canFinish(int numCourses, int[][] prerequisites) {
-//         Map<Integer, List<Integer>> courses = new HashMap();
-//         // int[] indegree = new int[numCourses];
-//         for(int[] prereq: prerequisites){
-//             int a = prereq[0];
-//             int b = prereq[1];
-//             if(!courses.containsKey(a))
-//                 courses.put(a, new ArrayList<>(b));
-//             else courses.put(a, courses.get(a).add(b));
-//             // indegree[a]++;
-//         }
-
-//         boolean visited[] = new boolean[numCourses];
-//         for(int i =0; i<numCourses; i++){
-//             if(visited[i] == false){
-//                 boolean possible = dfs(courses, visited);
+//         for(int i=0; i<prerequisites.length; i++){
+//             int course = prerequisites[i][0];
+//             Integer prereq_ = prerequisites[i][1];
+//             if (!map.containsKey(course)) {
+//                 ArrayList<Integer> list = new ArrayList<>();
+//                 list.add(prereq_);
+//                 map.put(course, list);
 //             }
+//             else{
+//                 map.get(course).add(prereq_);
+//             }            
 //         }
-        
-//         // Queue<Integer> q = new LinkedList();
-//         // int count =0;
-//         // while(!q.isEmpty()){
-//         //     Map<Integer, List<Integer>> curr = q.poll();
-//         //     Integer currCourse = curr.getKey();
-//         //     List<Integer> currPrereqs = curr.getValue();
-//         //     for(int i = 0; i < currPrereqs.size(); i++){
-//         //         if(visited.contains(currPrereqs.get(i)))return false;
-//         //         visited.add(currPrereqs.get(i));
-//         //         q.add()
-//         //     }
-//         // }
+//         boolean[] visitedAllCourses = new boolean[numCourses];
+//         boolean[] visitedCurrentCourseNeighbors = new boolean[numCourses];
+
+//         for(int course = 0; course<numCourses; course++){
+//             if(visitedAllCourses[course] == false && 
+//                dfs(course, visitedAllCourses, visitedCurrentCourseNeighbors) == false) 
+//                 return false;
+//         }
+//         return true;
 //     }
-//     public boolean dfs(Map<Integer, List<Integer>> courses, boolean[] visited){
-//         if(courses)
+//     boolean dfs(int course, boolean[] visitedAllCourses, boolean[] visitedCurrentCourseNeighbors){
+//         if(visitedCurrentCourseNeighbors[course])
+//             return false;
+//         if(visitedAllCourses[course])
+//             return true;
+
+//         visitedCurrentCourseNeighbors[course] = true;
+//         if(map.containsKey(course))
+//         for(Integer prereq_ : map.get(course)){
+//             if(dfs(prereq_, visitedAllCourses, visitedCurrentCourseNeighbors) == false) 
+//                 return false;
+//         }
+//         visitedAllCourses[course] = true;
+//         visitedCurrentCourseNeighbors[course] = false;
+//         return true;
 //     }
+
 // }
 
-// class Node{
-//     int course;
-//     List<Integer> prereq;
-//     Node(int val, prereq_){
-//         int course = val;
-//         prereq = new ArrayList(prereq_);
-//     }
-// }
 
+// BFS Solution
 class Solution {
-    HashMap<Integer, ArrayList<Integer>> map = new HashMap();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        for(int i=0; i<prerequisites.length; i++){
-            int course = prerequisites[i][0];
-            Integer prereq_ = prerequisites[i][1];
-            if (!map.containsKey(course)) {
-                ArrayList<Integer> list = new ArrayList<>();
-                list.add(prereq_);
-                map.put(course, list);
+        // Adj List
+        List<List<Integer>> adj = new ArrayList();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Indegree
+        int[] inDegree = new int[numCourses];
+        for(int[] prereq : prerequisites){
+            adj.get(prereq[1]).add(prereq[0]);
+            inDegree[prereq[0]]++;
+        }
+
+        //Queue Initialization with inDegree ==0
+        Queue<Integer> que = new LinkedList();
+        for(int i=0; i<numCourses; i++)
+            if(inDegree[i] == 0)
+                que.add(i);
+
+        //BFS by counting the courses that can be taken
+        int count = 0;
+        while(!que.isEmpty()){
+            int currentCourse = que.poll();
+            count++;
+
+            for(int neighbor : adj.get(currentCourse)){
+                inDegree[neighbor]--;
+                if(inDegree[neighbor] == 0)
+                    que.add(neighbor);
             }
-            else{
-                map.get(course).add(prereq_);
-            }            
-        }
-        boolean[] visitedAllCourses = new boolean[numCourses];
-        boolean[] visitedCurrentCourseNeighbors = new boolean[numCourses];
 
-        for(int course = 0; course<numCourses; course++){
-            if(visitedAllCourses[course] == false && 
-               dfs(course, visitedAllCourses, visitedCurrentCourseNeighbors) == false) 
-                return false;
         }
-        return true;
-    }
-    boolean dfs(int course, boolean[] visitedAllCourses, boolean[] visitedCurrentCourseNeighbors){
-        if(visitedCurrentCourseNeighbors[course])
-            return false;
-        if(visitedAllCourses[course])
-            return true;
 
-        visitedCurrentCourseNeighbors[course] = true;
-        if(map.containsKey(course))
-        for(Integer prereq_ : map.get(course)){
-            if(dfs(prereq_, visitedAllCourses, visitedCurrentCourseNeighbors) == false) 
-                return false;
-        }
-        visitedAllCourses[course] = true;
-        visitedCurrentCourseNeighbors[course] = false;
-        return true;
+        //If count in queue same as courses returns true
+        return count == numCourses;
     }
 
 }
