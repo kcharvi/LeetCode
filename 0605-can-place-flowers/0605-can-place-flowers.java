89c218a0a8bb90d560f0087f12b612b6b5a7f83d
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-03 16:29:19 -0500

    Time: 1 ms (97.53%), Space: 45.7 MB (44.6%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,51 +1,31 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        // if(n == 0)return true;
-        // if(flowerbed.length == 1){
-        //     if(n == 1){
-        //         if(flowerbed[0] == 0)return true;
-        //         else return false;
-        //     }
-        //     else return true;
-        // }
-        // int count = 0;
-        // int i = 0;
-        // while(i<flowerbed.length){
-        //     if(count == n)return true;
-        //     if(i==0){
-        //         if(flowerbed[i+1] !=1 && flowerbed[i] == 0){
-        //             flowerbed[i] = 1;
-        //             count++;
-        //         }
-        //     }
-        //     if(i==flowerbed.length-1){
-        //         if(flowerbed[i-1] !=1 && flowerbed[i] == 0){
-        //             flowerbed[i] = 1;
-        //             count++;
-        //         }
-        //     }
-        //     else {
-        //         if(flowerbed[i-1] != 1 && flowerbed[i+1] !=1 && flowerbed[i] == 0){
-        //                 flowerbed[i] = 1;
-        //                 count++;
-        //             }
-        //         i++;
-        //     }
-        // }
-        // return false;
-        if (n == 0) return true;
-
-        int count = 0;
-        for (int i = 0; i < flowerbed.length; i++) {
-            if (flowerbed[i] == 0 &&
-                (i == 0 || flowerbed[i - 1] == 0) &&
-                (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
-                flowerbed[i] = 1;
-                count++;
-                if (count >= n) return true; 
-            }
+        int len = flowerbed.length;
+        //Base Case
+        if(len==1){
+            if(flowerbed[0] == 0 && n==1 || n==0)return true;
+            else return false;
         }
-
-        return count >= n;
+        int count = n;
+        for(int i=0; i<len-1; i++){
+            if(i==0){
+                if(flowerbed[i] == 0 && flowerbed[i+1] == 0){
+                    flowerbed[i] = 1;
+                    count--;
+                }
+                continue;
+            }
+            if(flowerbed[i]==0){ 
+                if(flowerbed[i-1]==0 && flowerbed[i+1]==0){
+                    flowerbed[i] = 1;
+                    count--;
+                }
+            }
+                    
+        }
+        if(flowerbed[len-1]==0 && flowerbed[len-2]==0){
+            count--;
+        }
+        return count<=0 ? true:false;
     }
 }
