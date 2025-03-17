c93bfc5524d7b89e464cab7becdcf1ce4a630cc8
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-17 13:54:44 -0400

    Time: 8 ms (88.95%), Space: 47.4 MB (62%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.java
@@ -13,37 +13,67 @@
  *     }
  * }
  */
+
+// SOLUTION 1:
+// class Solution {
+//     HashMap<Integer,ArrayList<Integer>> map = new HashMap(); 
+//     public int maxLevelSum(TreeNode root) {
+//         //bfs populate the map
+//         populateBFS(root, 0);
+//         int maxSum = Integer.MIN_VALUE;
+//         int maxKey = 0;
+//         for(Map.Entry<Integer, ArrayList<Integer>> entry: map.entrySet()){
+//             int sum = 0;
+//             for(Integer i: entry.getValue()){
+//                 sum+=i;
+//             }
+//             if(sum>maxSum){
+//                 maxSum = sum;
+//                 maxKey = entry.getKey();
+//             }
+//         }
+//         return maxKey+1;
+//     }
+//     void populateBFS(TreeNode node, int depth){
+//         if(node == null)return;
+//         if(map.containsKey(depth)){
+//             ArrayList<Integer> list = map.get(depth);
+//             list.add(node.val);
+//         }
+//         else{
+//             ArrayList<Integer> list = new ArrayList();
+//             list.add(node.val);
+//             map.put(depth, list);
+//         }
+//         populateBFS(node.left, depth+1);
+//         populateBFS(node.right, depth+1);
+//     }
+// }
+
+// SOLUTION 2:
 class Solution {
-    HashMap<Integer,ArrayList<Integer>> map = new HashMap(); 
     public int maxLevelSum(TreeNode root) {
-        //bfs populate the map
-        populateBFS(root, 0);
+        int maxLevel = 1;
+        int level = 1;
         int maxSum = Integer.MIN_VALUE;
-        int maxKey = 0;
-        for(Map.Entry<Integer, ArrayList<Integer>> entry: map.entrySet()){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int currsize = queue.size();
             int sum = 0;
-            for(Integer i: entry.getValue()){
-                sum+=i;
+            for(int i=0; i<currsize; i++){
+                TreeNode node = queue.remove();
+                sum+=node.val;
+                if(node.left!=null)queue.add(node.left);
+                if(node.right!=null)queue.add(node.right);
             }
             if(sum>maxSum){
                 maxSum = sum;
-                maxKey = entry.getKey();
+                maxLevel = level;
             }
+            level++;
         }
-        return maxKey+1;
+        return maxLevel;
     }
-    void populateBFS(TreeNode node, int depth){
-        if(node == null)return;
-        if(map.containsKey(depth)){
-            ArrayList<Integer> list = map.get(depth);
-            list.add(node.val);
-        }
-        else{
-            ArrayList<Integer> list = new ArrayList();
-            list.add(node.val);
-            map.put(depth, list);
-        }
-        populateBFS(node.left, depth+1);
-        populateBFS(node.right, depth+1);
-    }
+        
 }
