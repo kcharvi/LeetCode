3029d8c9acd4a0c34566cdc6b19cbfb8026c3944
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-19 09:28:27 -0400

    Time: 0 ms (100%), Space: 45 MB (96.81%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -15,17 +15,23 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode curr = q.remove();
-            int qsize = q.size();
-            if(curr.val == val)
-                return curr;
-            if(curr.left!=null)q.add(curr.left);
-            if(curr.right!=null)q.add(curr.right);
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
+        // while(!q.isEmpty()){
+        //     TreeNode curr = q.remove();
+        //     int qsize = q.size();
+        //     if(curr.val == val)
+        //         return curr;
+        //     if(curr.left!=null)q.add(curr.left);
+        //     if(curr.right!=null)q.add(curr.right);
+        // }
+        // return null;
+
+        // BINARY SEARCH TREE PROPERTY
+        while(root != null && root.val != val){
+            root = val < root.val ? root.left:root.right;
         }
-        return null;
+       return root;
 
     }
 }
