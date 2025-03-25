7e86fec8ffad2cefe74dcc1c9075fee90fd2d38f
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-25 11:38:59 -0400

    Time: 0 ms (100%), Space: 42 MB (78.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,34 +1,40 @@
 class Solution {
-    
+    List<String> ans = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        if(digits=="" || digits==null || digits.length()==0)
-            return new ArrayList<>();
+        if (digits.isEmpty()) 
+            return ans;
         
-        Map<Character, String> map = new HashMap();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
-        
-        String[] strs = new String[digits.length()];
-        for(int i =0; i<digits.length();i++)
-            strs[i]=map.get(digits.charAt(i));
-        
-        String[] arr = strs[0].split(""); // 2 - abc   arr=["a","b","c"]
-        for(int i=1; i<strs.length; i++)
-            arr = getStrings(arr, strs[i]);   // (arr=["a","b","c"], strs=["def"])
-        return Arrays.asList(arr);
+        HashMap<Character, String> lettermap = new HashMap<>();
+        lettermap.put('2', "abc");
+        lettermap.put('3', "def");
+        lettermap.put('4', "ghi");
+        lettermap.put('5', "jkl");
+        lettermap.put('6', "mno");
+        lettermap.put('7', "pqrs");
+        lettermap.put('8', "tuv");
+        lettermap.put('9', "wxyz");
+
+        backtrack(lettermap, digits, 0, new StringBuilder());
+        return ans;
     }
-    public String[] getStrings(String[] arr, String str){
-        String[] temp = new String[arr.length * str.length()];  // arr=3 * str=3  which is 9
-        int k=0;
-        for(int i=0;i<arr.length;i++)
-            for(int j=0; j<str.length();j++)
-                temp[k++] = arr[i].concat(String.valueOf(str.charAt(j)));
-        return temp;
+    void backtrack(HashMap<Character, 
+                   String> lettermap, 
+                   String digits, 
+                   int idx, 
+                   StringBuilder formedsofar){
+        if(formedsofar.length() == digits.length()){
+            ans.add(formedsofar.toString());
+            return;
+        }
+        
+        if(idx<digits.length()){
+            char ch = digits.charAt(idx);
+            String available = lettermap.get(ch);
+            for(int j=0; j<available.length(); j++){
+                formedsofar.append(available.charAt(j));
+                backtrack(lettermap, digits, idx+1, formedsofar);
+                formedsofar.deleteCharAt(formedsofar.length() - 1);
+            }
+        }
     }
 }
