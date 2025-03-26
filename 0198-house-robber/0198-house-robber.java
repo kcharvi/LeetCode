060f02015cb74e3df233ea3debbfc83ec48abe9a
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-26 11:15:58 -0400

    Time: 0 ms (100%), Space: 40.9 MB (88.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,12 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
+        if(n==1)return nums[0];
+        // if(n==2)return Math.max(nums[0], nums[1]);
         int[] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = nums[0];
-        for(int i=2;i<=n; i++){
-            dp[i] = Math.max(nums[i-1]+dp[i-2], 
-                             dp[i-1]);
+        for(int i=2; i<=n; i++){
+            dp[i] = Math.max(dp[i-2]+nums[i-1], dp[i-1]);
         }
         return dp[n];
     }
