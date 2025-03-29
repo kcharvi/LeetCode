abb3b600c891963f85cba9c649ac6d844f209875
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-29 12:07:03 -0400

    Time: 0 ms (100%), Space: 40.8 MB (14.56%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,28 +1,45 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        String a_ = Integer.toBinaryString(a);
-        String b_ = Integer.toBinaryString(b);
-        String c_ = Integer.toBinaryString(c);
-        int maxlength = Math.max(a_.length(), Math.max(b_.length(), c_.length()));
-        a_ = String.format("%" + maxlength + "s", a_).replace(' ', '0');
-        b_ = String.format("%" + maxlength + "s", b_).replace(' ', '0');
-        c_ = String.format("%" + maxlength + "s", c_).replace(' ', '0');
+        // String a_ = Integer.toBinaryString(a);
+        // String b_ = Integer.toBinaryString(b);
+        // String c_ = Integer.toBinaryString(c);
+        // int maxlength = Math.max(a_.length(), Math.max(b_.length(), c_.length()));
+        // a_ = String.format("%" + maxlength + "s", a_).replace(' ', '0');
+        // b_ = String.format("%" + maxlength + "s", b_).replace(' ', '0');
+        // c_ = String.format("%" + maxlength + "s", c_).replace(' ', '0');
+        // int count = 0;
+        // for(int i=0; i<c_.length(); i++){
+        //     char bit1 = a_.charAt(i);
+        //     char bit2 = b_.charAt(i);
+        //     char res = c_.charAt(i);
+        //     char orResult = (char) ((bit1 - '0') | (bit2 - '0') + '0');
+        //     if(orResult != res){
+        //         if(res == '0'){
+        //             if(bit1 == '1' && bit2 == '1')
+        //                 count+=2;
+        //             else 
+        //                 count++;
+        //         }
+        //         else{
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
         int count = 0;
-        for(int i=0; i<c_.length(); i++){
-            char bit1 = a_.charAt(i);
-            char bit2 = b_.charAt(i);
-            char res = c_.charAt(i);
-            char orResult = (char) ((bit1 - '0') | (bit2 - '0') + '0');
-            if(orResult != res){
-                if(res == '0'){
-                    if(bit1 == '1' && bit2 == '1')
+        for(int i=0; i<32; i++){
+            int bita = (a>>i)&1;
+            int bitb = (b>>i)&1;
+            int bitc = (c>>i)&1;
+            if((bita | bitb) != bitc){
+                if(bitc == 0){
+                    if((bita & bitb) == 1)
                         count+=2;
-                    else 
-                        count++;
-                }
-                else{
+                    else count++;
+                }                    
+                else
                     count++;
-                }
             }
         }
         return count;
