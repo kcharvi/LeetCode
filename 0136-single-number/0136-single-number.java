ec5727ace810a3a85014aa1445887e04be0273e1
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-03-29 11:17:39 -0400

    Time: 1 ms (99.86%), Space: 46.4 MB (25.53%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor =0;
-        for(int i:nums)xor^=i;
+        int xor = 0;
+        for(int i: nums)
+            xor = xor^i;
         return xor;
     }
 }
