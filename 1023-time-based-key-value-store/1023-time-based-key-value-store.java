fa2714e9707541c495f468210fc1d84eba2e6908
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-10 14:05:39 -0400

    Time: 162 ms (17.91%), Space: 122.1 MB (32.6%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.java
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -1,47 +1,70 @@
-class TimeValue {
-    int timestamp;
-    String value;
-    TimeValue(int timestamp, String value) {
-        this.timestamp = timestamp;
-        this.value = value;
-    }
-}
+// class TimeValue {
+//     int timestamp;
+//     String value;
+//     TimeValue(int timestamp, String value) {
+//         this.timestamp = timestamp;
+//         this.value = value;
+//     }
+// }
+// class TimeMap {
+//     HashMap<String, List<TimeValue>> map;
+//     public TimeMap() {
+//         this.map = new HashMap();
+//     }
+    
+//     public void set(String key, String value, int timestamp) {
+//         map.putIfAbsent(key, new ArrayList<>());
+//         map.get(key).add(new TimeValue(timestamp, value));
+//     }
+    
+//     public String get(String key, int timestamp) {
+//         if(!map.containsKey(key)) return "";
+//         List<TimeValue> valueTimePairs = map.get(key);
+        
+//         //Binary Search on the valueTimePairs
+//         int l = 0;
+//         int r = valueTimePairs.size()-1;
+//         String result = "";
+//         while(l<=r){
+//             int mid = l+(r-l)/2;
+//             if(valueTimePairs.get(mid).timestamp == timestamp){
+//                 return valueTimePairs.get(mid).value;
+//             }
+//             if(valueTimePairs.get(mid).timestamp < timestamp){
+//                 result = valueTimePairs.get(mid).value;
+//                 l = mid+1;
+//             }
+//             else{
+//                 r = mid-1;
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
+
+// SOLUTION 2
+
 class TimeMap {
-    HashMap<String, List<TimeValue>> map;
+    HashMap<String, TreeMap<Integer, String>> map;
     public TimeMap() {
         this.map = new HashMap();
     }
-    
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key, new ArrayList<>());
-        map.get(key).add(new TimeValue(timestamp, value));
+        map.putIfAbsent(key, new TreeMap<>());
+        map.get(key).put(timestamp, value);
     }
-    
+
     public String get(String key, int timestamp) {
         if(!map.containsKey(key)) return "";
-        List<TimeValue> valueTimePairs = map.get(key);
-        
-        //Binary Search on the valueTimePairs
-        int l = 0;
-        int r = valueTimePairs.size()-1;
-        String result = "";
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(valueTimePairs.get(mid).timestamp == timestamp){
-                return valueTimePairs.get(mid).value;
-            }
-            if(valueTimePairs.get(mid).timestamp < timestamp){
-                result = valueTimePairs.get(mid).value;
-                l = mid+1;
-            }
-            else{
-                r = mid-1;
-            }
-        }
-        return result;
+        TreeMap<Integer, String> timeMap = map.get(key);
+        Integer floorKey = timeMap.floorKey(timestamp);
+        return floorKey == null ? "" : timeMap.get(floorKey);
     }
-}
 
+
+}
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
