338e729744717e93448113a0a837a1d42637e6e6
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-14 11:43:14 -0400

    Time: 0 ms (100%), Space: 42.9 MB (58.95%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -4,13 +4,13 @@ class Solution {
         int r = arr.length-1;
         int missing = 0;
         while(l<=r){
-            int mid = l+(r-l)/2; //1 , 3
-            missing = arr[mid] - (mid+1);
-            if(missing < k)
-                l=mid+1;
+            int mid = l+(r-l)/2; //2, 3, 4
+            missing = arr[mid] - (mid+1); //4-3=1, 7-4=3, 11-5=6
+            if(missing < k) 
+                l=mid+1;  //3, 4
             else
-                r=mid-1;
+                r=mid-1; //3
         }
-        return l+k;
+        return l+k;  //4+5
     }
 }
