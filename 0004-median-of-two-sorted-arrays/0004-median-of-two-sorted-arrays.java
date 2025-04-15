0e1e67c261c8c30e4f8c688ac412120083153501
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-15 15:39:26 -0400

    Time: 2 ms (56.49%), Space: 46.2 MB (44.67%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,28 +1,64 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length;
-        int m=nums2.length;
-        ArrayList<Integer> al=new ArrayList<>();
-         int[] arr=new int[n+m];
-        for(int i=0;i<n;i++){
-            al.add(nums1[i]);
+        int m = nums1.length;
+        int n = nums2.length;
+        int total = m + n;
+
+        int i = 0, j = 0;
+        int prev = 0, curr = 0;
+
+        for (int count = 0; count <= total / 2; count++) {
+            prev = curr;
+
+            if (i < m && (j >= n || nums1[i] <= nums2[j])) {
+                curr = nums1[i++];
+            } else {
+                curr = nums2[j++];
+            }
         }
-        for(int i=0;i<m;i++){
-            al.add(nums2[i]);
+
+        if (total % 2 == 0) {
+            return (prev + curr) / 2.0;
+        } else {
+            return curr;
         }
-        for(int i=0;i<n+m;i++){
-            arr[i]=al.get(i);
-        }
-        Arrays.sort(arr);
-       
         
-        int low=0;
-        int high=arr.length-1;
-        int mid=(low+(high-low))/2;
-        if((n+m)%2!=0){
-            return (double)arr[mid];
-        }else{
-            return (double)(arr[mid]+arr[mid+1])/2;
-        }
+        
+        
+        //     int m = nums1.length;
+        //     int n = nums2.length;
+
+        //     //median of nums1
+        //     int l = 0;
+        //     int r1 = m-1;
+        //     double median1 = 0;
+        //     double median2 = 0;
+        //     int r2 = n-1;
+
+        //     if(r1==0 || r2==0){
+        //         if(r1==0)return medianCalculator(0, r2, nums2);
+        //         else return medianCalculator(0, r1, nums1);
+        //     }
+
+        //     median1 = medianCalculator(0, r1, nums1);
+        //     median2 = medianCalculator(0, r2, nums2);
+            
+        //     return (double)  (median1+median2)/2;
+        // }
+
+        // double medianCalculator(int l, int r, int[] nums){
+        //     double median = 0;
+        //     if(nums.length == 0)return 0;
+        //     if(nums.length == 1)return nums[0];
+        //     if(nums.length % 2 == 0){
+        //         int mid = l+(r-l)/2;
+        //         int mid2 = mid+1;
+        //         median = (double)(nums[mid]+nums[mid2])/2;
+        //     }
+        //     else{
+        //         int mid = l+(r-l)/2;
+        //         median = (double) nums[mid];
+        //     }
+        //     return median;
     }
 }
