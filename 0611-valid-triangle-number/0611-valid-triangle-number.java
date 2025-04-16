f038e1c7c8aac63ae15d0d15089af76da1d0c9bf
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-16 13:49:15 -0400

    Time: 24 ms (61.96%), Space: 43.7 MB (16.43%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -3,14 +3,24 @@ class Solution {
         if(nums.length < 3)return 0;
         Arrays.sort(nums);
         int count=0;
-        for(int i = 0; i<nums.length-2; i++){
-            if(nums[i] == 0)continue;
-            for(int j = i+1; j<nums.length-1; j++){
-                for(int k = j+1; k<nums.length; k++){
-                    if(nums[i]+nums[j] > nums[k])
-                        count++;
+        for(int k = nums.length-1; k>=2; k--){
+            // if(nums[i] == 0)continue;
+            // for(int j = i+1; j<nums.length-1; j++){
+            //     for(int k = j+1; k<nums.length; k++){
+            //         if(nums[i]+nums[j] > nums[k])
+            //             count++;
+            //     }
+            // }
+            int i = 0;
+            int j = k-1;
+            while(i<j){
+                if(nums[i]+nums[j] > nums[k]){
+                    count+=(j-i);
+                    j--;
                 }
+                else i++;
             }
+
         }
         return count;
     }
