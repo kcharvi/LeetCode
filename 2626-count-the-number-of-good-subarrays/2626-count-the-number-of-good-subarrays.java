1f377ca10465ea96221103ca28c2714708f96337
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-16 17:05:30 -0400

    Time: 57 ms (47.84%), Space: 60.6 MB (30.23%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.java
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.java
@@ -3,43 +3,18 @@ class Solution {
         long ans = 0;
         int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap();
-        long count_of_pairs = 0;
+        int pairs = 0;
         int l = 0;
         for(int r=0; r<n; r++){
-            int right_val = nums[r];           
-            int freq_right_val = map.getOrDefault(right_val, 0);
-            count_of_pairs += freq_right_val; 
-            map.put(right_val, freq_right_val+1);
+            pairs += map.getOrDefault(nums[r], 0); 
+            map.put(nums[r], map.getOrDefault(nums[r], 0)+1);
             
-            while(count_of_pairs >= k){
-                ans += n-r;
-                int left_val = nums[l];
-                int freq_left_val = map.get(left_val);
-                map.put(left_val, freq_left_val-1);
-                count_of_pairs -= freq_left_val-1;
+            while(pairs >= k){  //shrink the window from left and remove the freq from map
+                map.put(nums[l], map.get(nums[l])-1);
+                pairs -= map.get(nums[l]);
                 l++;
             }
-
-            // for(int r=l+1; r<n; r++){
-            //     int curr_right_val = nums[r];
-
-            //     // count the pairs in window l to r   
-            //     if(map.containsKey(curr_right_val)){
-            //         map.put(curr_right_val, map.get(curr_right_val)+1);
-            //         count_of_pairs = map.get(curr_right_val)*(map.get(curr_right_val)-1)/2;
-            //     }  
-            //     else{
-            //         map.put(curr_right_val, 1);
-            //     }                           
-            //     if(count_of_pairs >= k)
-            //         ans++;
-            // }
-            // if(map.get(curr_left_val) > 1){
-            //     map.put(curr_left_val, map.get(curr_left_val)-1);
-            // }
-            // else{
-            //     map.remove(curr_left_val);
-            // }
+            ans+=l;
         }
         return ans;        
     }
