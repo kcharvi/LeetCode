ac0f33aa0830216a77a3a48e02d8fbc80edf8957
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-20 11:25:33 -0400

    Time: 19 ms (57.35%), Space: 45.3 MB (41.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
-        if (n == 0) return "";
-
-        int[][] dp = new int[n][n];
-        int maxLength = 1;
-        int strt = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = 1; 
-        }
-        
-        for(int len = 2; len <= n; len++){
-            for (int i = 0; i < n - len + 1; i++) {
-                int j = i + len - 1;
-                if(s.charAt(i) == s.charAt(j)) {
-                    if (len == 2) {
-                        dp[i][j] = 1;
-                    } else if (dp[i + 1][j - 1] == 1) {
-                        dp[i][j] = 1;
-                    }
-                    if (dp[i][j] == 1 && len > maxLength) {
-                        maxLength = len;
-                        strt = i;
-                    }
+        if(n<=1)return s;
+        String str = s.substring(0,1);
+        int strLen = 1;
+        int maxLen = 1;
+        for(int i = 0; i<n; i++){
+            int l = i-1;
+            int r = i+1;
+            while(l>=0 && r<n && s.charAt(l) == s.charAt(r)){
+                if(r-l+1 > strLen){
+                    strLen = r-l+1;
+                    str = s.substring(l, r+1);
                 }
+                l--;
+                r++;
+            }
+
+            int le = i;
+            int ri = i+1;
+            while(le>=0 && ri<n && s.charAt(le) == s.charAt(ri)){
+                if(ri-le+1 > strLen){
+                    strLen = ri-le+1;
+                    str = s.substring(le, ri+1);
+                }
+                le--;
+                ri++;
             }
         }
-        return s.substring(strt, strt+maxLength);
+        return str;
     }
 }
