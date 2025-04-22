d9ba4a7322342a94c409b976c9e56bd62eecee1d
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-22 16:19:03 -0400

    Time: 8 ms (6.09%), Space: 43.5 MB (35.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,18 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList();
-        backtrack(0, 0, n, ans, "");
-        return ans;
-    }
-    public void backtrack(int opencount, int closecount, int n, List<String> ans, String curr){
-        if(curr.length()==2*n)
-            ans.add(curr);
+        List<List<String>> dp = new ArrayList();
+        for(int i=0; i<=n; i++)
+            dp.add(new ArrayList());
         
-        if(opencount<n){
-            backtrack(opencount+1, closecount, n, ans, curr+"(");
+        dp.get(0).add("");
+
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                List<String> curr = new ArrayList();
+                for(String x: dp.get(j))
+                    for(String y: dp.get(i-j-1))
+                        curr.add("("+x+")"+y);
+                dp.get(i).addAll(curr);
+            }
         }
-        if(closecount<opencount){
-            backtrack(opencount, closecount+1, n, ans, curr+")");
-        }
+
+        return dp.get(n);   
     }
 }
