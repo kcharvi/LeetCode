62791992bc2ebb7c13dc2b86a4ff7c73e91114c5
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-24 13:52:22 -0400

    Time: 378 ms (5.16%), Space: 45.6 MB (18.03%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
         if(nums.length==1)return 0;
-        int jumps=0;
-        int reach_so_far=0;
-        int max_reach=0;
-        for(int i =0; i<nums.length-1;i++){
-            max_reach=Math.max(max_reach,nums[i]+i);
-            if(reach_so_far==i){
-                jumps++;
-                reach_so_far=max_reach;
-            }
-        }
-        return jumps;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
 
+        // dp = [0, 1, 1, 2, 2] -> dp[i] 
+        for(int i=1; i<nums.length; i++){
+            for(int j=0; j<i; j++){
+                if(j+nums[j] >= i && dp[j] != Integer.MAX_VALUE) //if the current position i is reachable from j and j itself has been reachable
+                    dp[i] = Math.min(dp[j]+1, dp[i]); // minimum steps to reach i from j
+            }
+            
+        }
+        return dp[nums.length-1];
     }
 }
