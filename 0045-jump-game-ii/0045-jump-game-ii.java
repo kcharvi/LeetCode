8f5ea982bec76bec1a6abdd81c54a7fbf286d837
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-24 14:13:36 -0400

    Time: 1 ms (99.49%), Space: 45.2 MB (50.65%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,11 +1,21 @@
 class Solution {
     public int jump(int[] nums) {
-
-    // POINTERS SOLUTION    
+    
+    // GREEDY - POINTERS SOLUTION 
+    // Greedy is perfect here because:
+    // It turns out the minimum number of jumps always occurs when you 
+    // take the farthest next step at the right time.
+    // I can go anywhere between i and end. Let me explore all those 
+    // options and pick the farthest I can reach from here. Once I reach 
+    // the end of my current range, I jump to the next range.   
         if(nums.length==1)return 0;
         int jumps = 0;
         int end = 0;
         int farthest = 0;
+        //        i =       0       ,     1      ,     2      ,   
+        // farthest = 0, max(2,0)=2 , max(4,2)=4 , max(3,4)=4 ,
+        // jumps    = 0,    1       ,     1      ,     2      ,
+        // end      = 0,    2       ,     2      ,     4      ,
         for(int i=0; i<nums.length-1; i++){
             farthest = Math.max(i+nums[i], farthest);
             if(i == end){
@@ -17,6 +27,9 @@ class Solution {
 
 
     // DP SOLUTION
+    // Explore all ways to reach a position
+    // At each step i, you ask:
+    // "Who can reach me, and among those, who can get to me with the fewest jumps?"
         // if(nums.length==1)return 0;
         // int[] dp = new int[nums.length];
         // Arrays.fill(dp, Integer.MAX_VALUE);
@@ -29,8 +42,6 @@ class Solution {
         //     }   
         // }
         // return dp[nums.length-1];
-
-
 
     }
 }
