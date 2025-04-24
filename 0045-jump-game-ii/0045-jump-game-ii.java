fc911b25cb1153ea85e27ddee8bed4118d60cc90
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-24 14:01:08 -0400

    Time: 1 ms (99.49%), Space: 45.4 MB (40.91%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,18 +1,36 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length==1)return 0;
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
 
-        // dp = [0, 1, 1, 2, 2] -> dp[i] 
-        for(int i=1; i<nums.length; i++){
-            for(int j=0; j<i; j++){
-                if(j+nums[j] >= i && dp[j] != Integer.MAX_VALUE) //if the current position i is reachable from j and j itself has been reachable
-                    dp[i] = Math.min(dp[j]+1, dp[i]); // minimum steps to reach i from j
+    // POINTERS SOLUTION    
+        if(nums.length==1)return 0;
+        int jumps = 0;
+        int end = 0;
+        int farthest = 0;
+        for(int i=0; i<nums.length-1; i++){
+            farthest = Math.max(i+nums[i], farthest);
+            if(i == end){
+                jumps++;
+                end = farthest;
             }
-            
         }
-        return dp[nums.length-1];
+        return jumps;
+
+
+    // DP SOLUTION
+        // if(nums.length==1)return 0;
+        // int[] dp = new int[nums.length];
+        // Arrays.fill(dp, Integer.MAX_VALUE);
+        // dp[0] = 0;
+        // // dp = [0, 1, 1, 2, 2] -> dp[i] 
+        // for(int i=1; i<nums.length; i++){
+        //     for(int j=0; j<i; j++){
+        //         if(j+nums[j] >= i && dp[j] != Integer.MAX_VALUE) //if the current position i is reachable from j and j itself has been reachable
+        //             dp[i] = Math.min(dp[j]+1, dp[i]); // minimum steps to reach i from j
+        //     }   
+        // }
+        // return dp[nums.length-1];
+
+
+
     }
 }
