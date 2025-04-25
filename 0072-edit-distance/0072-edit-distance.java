cad28d06fe4dd278afe0df9a6601121aaa6bdcfc
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-25 14:35:52 -0400

    Time: 5 ms (64.5%), Space: 45.5 MB (11.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,14 +2,12 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        if(m == 0 || n == 0)
-            return Math.max(m, n);
+        if(m == 0)return n;
+        if(n == 0)return m;
         int[][] dp = new int[m+1][n+1];
-
-        for(int i = 0; i<=m; i++)
-            dp[i][0] = i;
-        for(int i = 0; i<=n; i++)
-            dp[0][i] = i;
+        dp[0][0] = 0;
+        for(int i=1; i<=m; i++)dp[i][0] = i;
+        for(int i=1; i<=n; i++)dp[0][i] = i;
         
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
@@ -17,13 +15,10 @@ class Solution {
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else{
-                    dp[i][j] = 1+Math.min(Math.min(dp[i-1][j], dp[i][j-1]), 
-                                          dp[i-1][j-1]);
+                    dp[i][j] = 1+ Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
                 }
             }
         }
         return dp[m][n];
-
-
     }
 }
