ea11e244c8ce94b83da67c224704ff5b6b649351
Charvi Kusuma <53350340+kcharvi@users.noreply.github.com>
2025-04-27 15:09:29 -0400

    Time: 44 ms (73.02%), Space: 19.2 MB (66.07%) - LeetHub

--- 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.py
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.py
@@ -8,13 +8,24 @@ class Node:
 
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
-        def getMaxDepth(node) -> int:
-            if node is None:
-                return 0
-            maxi = 0
-            children = node.children
-            for child in children:
-                maxi = max(maxi, getMaxDepth(child))
-            return 1+maxi
-        return getMaxDepth(root)
-        
+        # def getMaxDepth(node) -> int:
+        #     if node is None:
+        #         return 0
+        #     maxi = 0
+        #     children = node.children
+        #     for child in children:
+        #         maxi = max(maxi, getMaxDepth(child))
+        #     return 1+maxi
+        # return getMaxDepth(root)
+        if not root:
+            return 0
+
+        queue = deque([(root, 1)])
+        while queue:
+            levels_so_far = len(queue)
+            for _ in range(levels_so_far):
+                current_node, level = queue.popleft()
+                for child in current_node.children:
+                    queue.append((child, level+1))
+                
+        return level
